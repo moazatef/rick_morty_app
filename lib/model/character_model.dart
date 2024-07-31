0197cbe1e3c9d5259f3b65271758407a26b5f3e2
Moaz Atef <moazsaady6@gmail.com>
2024-07-31 09:55:35 +0300

    add char model

---
+++ lib/model/character_model.dart
@@ -0,0 +1,29 @@
+class CharacterModel {
+  late final int id; //the id of the character. all of characters have unique id
+  late final String name; // the name of the character
+  late final String
+      status; // The status of the character ('Alive', 'Dead' or 'unknown').
+  late final String species; // The species of the character.
+  late final String type;
+  late final String image; // the image of the character
+
+  CharacterModel({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.species,
+    required this.type,
+    required this.image,
+  });
+
+  factory CharacterModel.fromjson(Map<String, dynamic> json) {
+    return CharacterModel(
+      id: json['id'],
+      name: json['name'],
+      status: json['status'],
+      species: json['species'],
+      type: json['type'],
+      image: json['image'],
+    );
+  }
+}
